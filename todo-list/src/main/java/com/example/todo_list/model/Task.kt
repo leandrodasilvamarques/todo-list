27cc2373711485a5564d9c4b15d43d3a7d24a2b7
leandrodasilvamarques <leandrodasilva01921@gmail.com>
2025-07-15 14:19:20 -0300

    Fix: Corrige configuração de remotes e repositório aninhado

---
+++ todo-list/src/main/java/com/example/todo_list/model/Task.kt
@@ -0,0 +1,22 @@
+package com.example.todo_list.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+@Entity
+data class Task(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @field:NotBlank(message = "A descrição da tarefa é obrigatoria.")
+    @field:Size(min = 3, message = "A descrição deve ter no minimo 3 caracteres.")
+    var descripition: String,
+
+    var completed: Boolean = false
+)
